a72ac05c7001cf43ebfbb38961f83286802fc9e0
Pranal404 <pranalinglerobotics@gmail.com>
2025-06-01 12:36:08 +1200

    Delete Tutlebot4 Code directory

--- Tutlebot4 Code/Ver 01/simple_launch.py
+++
@@ -1,9 +0,0 @@
-from launch import LaunchDescription
-from launch_ros.actions import Node
-
-def generate_launch_description():
-    return LaunchDescription([
-        Node(package='turtlebot_navigation',
-             executable='simple_autonomy',
-             name='simple_autonomy'),
-    ])
